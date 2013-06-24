7a5fc2b2d5017239bd3d881073717a3f044bc077
Adam Shiver <adam@elixirgraphics.com>
2013-06-24 15:19:17 -0400

    Cleaned up formatting

--- blogEntryTopper.js
+++ blogEntryTopper.js
@@ -1,26 +1,26 @@
-		/* 
-		
-		-=-= BLOG ENTRY TOPPER =-=-
-		
-		DESCRIPTION: 	Handles the blog entry topper images. Finds image in blog entries with an ALT tag
-									of 'blogEntryTopper' and then moves image to the top of the entry. The theme's 
-									CSS autosizes the image to fit the blog entry's width.
-						
-		AUTHOR: Adam Shiver
-		VERSION: 1.0b
-		DATE: May 9, 2011
-		
-		*/
-		
-		$(document).ready(function(){
+/* 
 
-			blogEntryTopperImg = $('.blog-entry img[alt^=blogEntryTopper]');
-			blogEntryTopperImg.hide();
-			blogEntryTopperImg.each(function() {
-				/* Applies special class to the image */
-				$(this).addClass('blogEntryTopperImg');  
-				/* Moves topper image to the top of the blog entry and wraps it in a div with a class of blogEntryTopper_wrapper which can be styled via CSS. */
-				$(this).prependTo($(this).parent().parent()).show().wrap('<div class="blogEntryTopper_wrapper" />');
-			});
+-=-= BLOG ENTRY TOPPER =-=-
 
-		});
+DESCRIPTION: 	Handles the blog entry topper images. Finds image in blog entries with an ALT tag
+							of 'blogEntryTopper' and then moves image to the top of the entry. The theme's 
+							CSS autosizes the image to fit the blog entry's width.
+				
+AUTHOR: Adam Shiver
+VERSION: 1.0b
+DATE: May 9, 2011
+
+*/
+
+$(document).ready(function(){
+
+	blogEntryTopperImg = $('.blog-entry img[alt^=blogEntryTopper]');
+	blogEntryTopperImg.hide();
+	blogEntryTopperImg.each(function() {
+		/* Applies special class to the image */
+		$(this).addClass('blogEntryTopperImg');  
+		/* Moves topper image to the top of the blog entry and wraps it in a div with a class of blogEntryTopper_wrapper which can be styled via CSS. */
+		$(this).prependTo($(this).parent().parent()).show().wrap('<div class="blogEntryTopper_wrapper" />');
+	});
+
+});
